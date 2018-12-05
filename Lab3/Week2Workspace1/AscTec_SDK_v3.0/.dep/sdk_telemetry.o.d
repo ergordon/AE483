cf090635701379e4d7d59db948bb6ea2caff240f
ergarcia2 <emilio123987@gmail.com>
2018-12-05 08:23:57 -0600

    Groundstation and labcode

---
+++ Lab3/Week2Workspace1/AscTec_SDK_v3.0/.dep/sdk_telemetry.o.d
@@ -0,0 +1,82 @@
+sdk_telemetry.o: sdk_telemetry.c main.h sdk.h LL_HL_comm.h \
+  jetiTelemetry.h sdk_telemetry.h gpsmath.h system.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/stdio.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/_ansi.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/newlib.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/config.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/ieeefp.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/stddef.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/stdarg.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/reent.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/_ansi.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/_types.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/lock.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/types.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/_types.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/limits.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/types.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/features.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/stdio.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/stdlib.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/stdlib.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/alloca.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/string.h \
+  c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/string.h
+
+main.h:
+
+sdk.h:
+
+LL_HL_comm.h:
+
+jetiTelemetry.h:
+
+sdk_telemetry.h:
+
+gpsmath.h:
+
+system.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/stdio.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/_ansi.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/newlib.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/config.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/ieeefp.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/stddef.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/stdarg.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/reent.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/_ansi.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/_types.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/lock.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/types.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/_types.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/include/limits.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/types.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/features.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/stdio.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/stdlib.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/machine/stdlib.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/alloca.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/string.h:
+
+c:\asctec_arm_sdk\winarm\bin\../lib/gcc/arm-elf/4.1.1/../../../../arm-elf/include/sys/string.h:
