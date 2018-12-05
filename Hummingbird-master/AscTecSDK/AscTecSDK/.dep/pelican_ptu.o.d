2b6e4a927609f331c64cf5e3d8fcd90c53430c46
ergarcia2 <emilio123987@gmail.com>
2018-12-05 08:35:20 -0600

    Removed Hummingbird Folder

--- Hummingbird-master/AscTecSDK/AscTecSDK/.dep/pelican_ptu.o.d
+++
@@ -1,9 +0,0 @@
-pelican_ptu.o: pelican_ptu.c main.h system.h pelican_ptu.h LPC214x.h
-
-main.h:
-
-system.h:
-
-pelican_ptu.h:
-
-LPC214x.h:
--- Hummingbird-master/AscTec_SDK_v3.0_with_usonic/.dep/lab.o.d
+++
@@ -1,7 +0,0 @@
-lab.o: lab.c lab.h main.h sdk.h
-
-lab.h:
-
-main.h:
-
-sdk.h:
